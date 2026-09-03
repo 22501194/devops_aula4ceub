d7cab17a0696294f29d79b279e9e8f64bef39e4d
Alice Paiva de Sá <alicedesa1806@dinfor.uniceub>
2026-09-03 09:20:25 -0300

    test:adiciona chave de API para teste de segredos

--- src/calculadora.js
+++ src/calculadora.js
@@ -6,7 +6,8 @@ function somar(a, b) {
 function subtrair(a, b) {
   return a - b;
 }
-
+// SIMULAÇÃO DE VAZAMENTO DE SEGREDO
+const AWS_SECRET_KEY = "AKIAIOSFODNN7EXAMPLE1234567890abcdef";
 module.exports = {
   somar,
   subtrair
