3a2294c3ecc0c278068f16f2e8970bb3bd5d6486
Alice Paiva de Sá <alicedesa1806@dinfor.uniceub>
2026-09-03 09:28:35 -0300

    test: adiciona dependencia antiga e vulnerável

--- src/calculadora.js
+++ src/calculadora.js
@@ -6,8 +6,7 @@ function somar(a, b) {
 function subtrair(a, b) {
   return a - b;
 }
-// SIMULAÇÃO DE VAZAMENTO DE SEGREDO
-const AWS_SECRET_KEY = "AKIAIOSFODNN7EXAMPLE1234567890abcdef";
+
 module.exports = {
   somar,
   subtrair
